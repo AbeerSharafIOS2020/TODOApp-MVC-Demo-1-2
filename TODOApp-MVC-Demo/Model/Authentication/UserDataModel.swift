b471b5bb4b086a70e4adea96164d63466d41b835
Abeer <p.ab_sh@icloud.com>
2020-12-06 11:38:25 +0300

    create UserDataModel model to save user data

---
+++ TODOApp-MVC-Demo/Model/Authentication/UserDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserDataModel.swift
+//  TODOApp-MVC-Demo
+//
+//  Created by AbeerSharaf on 12/6/20.
+//  Copyright © 2020 IDEAEG. All rights reserved.
+//
+
+import Foundation
+struct UserDataModel: Codable {
+var name, email: String
+var age: Int
+var password : String
+}
